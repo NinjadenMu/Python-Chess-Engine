bf690cef7c627f8bb2e5f0aca3abcc2ddb8d8c22
NinjadenMu <jaden.mu@gmail.com>
2021-08-08 16:57:14 -0400

    Update chess_engine.py

--- chess_engine.py
+++ chess_engine.py
@@ -1,6 +1,7 @@
 #import dependencies
 import chess
 import copy
+import chess.polyglot
 
 #Engine class, all functions are contained here
 class ChessEngine:
@@ -48,7 +49,7 @@ class ChessEngine:
 
     def eval(self, eval_board, board):
         if board.is_checkmate():
-            if board.turn == True:
+            if board.turn == False:
                 return -200000000
 
             else:
@@ -68,6 +69,11 @@ class ChessEngine:
         possible_moves = board.legal_moves
         return list(possible_moves)
 
+    def opening(self, board):
+        with chess.polyglot.open_reader("data/polyglot/performance.bin") as reader:
+            for entry in reader.find_all(board):
+                print(entry.move, entry.weight, entry.learn)
+
     def alphabeta(self, board, depth):
         moves = self.feed_moves(board)
 
@@ -79,13 +85,15 @@ class ChessEngine:
 
         initial_board = copy.deepcopy(board)
         move_scores = []
+
         for move in moves:
             board.push(move)
             move_scores.append(self.alphabeta(board, depth - 1))
             board = copy.deepcopy(initial_board)
  
         if board.turn == False and depth == self.initial_depth:
-            return moves[move_scores.index(max(move_scores))]
+            return (moves[move_scores.index(max(move_scores))], max(move_scores))
+
 
         elif board.turn == True: 
             return min(move_scores)
@@ -94,11 +102,12 @@ class ChessEngine:
             return max(move_scores)
 
     def run(self):
+        #print(self.opening(self.board))
         return self.alphabeta(copy.deepcopy(self.board), self.initial_depth)
 
     def make_move(self, move):
         self.board.push(move)
 
 if __name__ == '__main__':
-    engine = ChessEngine(chess.Board(), 8, -10)
-    engine.run()
+    engine = ChessEngine(chess.Board(), 3, -10)
+    print(engine.run())
