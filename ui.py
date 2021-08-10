07854a308ad4c76b638873d5d087710b7992fcc1
NinjadenMu <jaden.mu@gmail.com>
2021-08-09 20:34:03 -0400

    updated to support new chess_engine

--- ui.py
+++ ui.py
@@ -1,11 +1,16 @@
 #import dependencies
 import chess
 from chess_engine import ChessEngine
+import time
+#from stockfish import Stockfish
 
 #create board object
 board = chess.Board()
 chess_engine = ChessEngine(board, 3, 5)
+#stockfish = Stockfish(parameters={"Threads": 2})
+
 print('Input moves in the format starting square to ending square.  Do not add symbols for takes/check.  Castle with starting pos of king and ending pos of king')
+
 while board.outcome() == None: #repeat until the game reaches win/loss/draw
     print(board) #display board (needs to be printed each iteration of loop because board changes)
     #print('----------------')
@@ -15,15 +20,18 @@ while board.outcome() == None: #repeat until the game reaches win/loss/draw
     if move in [str(legal_move) for legal_move in list(board.legal_moves)]: #check if move is legal
         move = chess.Move.from_uci(move) #Convert input string to Move object
         board.push(move) #make move
+        start = time.perf_counter()
         engine_output = chess_engine.run()
-        print('Engine Eval: ' + str(engine_output[1]))
+        print('Time Taken: ' + str(time.perf_counter() - start))
+        print('Engine Eval: ' + str(-engine_output[1]))
+        #stockfish.set_fen_position(str(board.fen()))
+        #board.push(chess.Move.from_uci(stockfish.get_best_move_time(1500)))
         board.push(engine_output[0])
-        #chess_engine.make_move(move)
     
     else: #if move is not legal
         print('Invalid Move')
 
 if board.outcome() == chess.Outcome(termination=chess.Termination.CHECKMATE, winner=False):
-    print("I'm sorry, I'm think you missed it.")
+    print("I'm sorry, I think you missed it.")
 
 print('Thank you for a very enjoyable game.')
