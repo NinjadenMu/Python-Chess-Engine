1a1d7c95f0a47fc05e8745b10e39bd5b68dbcde0
yikuansun <yikuansun2015@gmail.com>
2021-08-20 17:39:29 -0400

    Use san for human input

--- ui.py
+++ ui.py
@@ -21,11 +21,12 @@ chess_engine = ChessEngine(board, ply, 5, False, threads)
 
 #stockfish = Stockfish(parameters={"Threads": 2})
 
-print('Input moves in the format starting square to ending square.  Do not add symbols for takes/check.  Castle with starting pos of king and ending pos of king')
+print('Input moves in standard algebraic notation.')
 while board.outcome() == None: #repeat until the game reaches win/loss/draw
     print(board) #display board (needs to be printed each iteration of loop because board changes)
     print('a b c d e f g h') #add letters under board
-    move = input('Your Move: ') #ask for human move
+    move = board.parse_san(input('Your Move: ')).uci() #ask for human move
+    print(move)
 
     if move in [str(legal_move) for legal_move in list(board.legal_moves)]: #check if move is legal
         move = chess.Move.from_uci(move) #Convert input string to Move object
