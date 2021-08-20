12366d38760677345a28a5048c446e2b74359ac2
NinjadenMu <jaden.mu@gmail.com>
2021-08-20 15:23:19 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,9 +5,10 @@ import time
 #from stockfish import Stockfish
 
 #create board object
-board = chess.Board()
+board = chess.Board('1q4k1/5pp1/2p3p1/K7/8/4n3/2r5/8 w - - 0 1')
 threads = int(input('How many threads may the engine use (should be less than cpu core count): '))
 chess_engine = ChessEngine(board, 4, 5, False, threads)
+ply = 4
 #stockfish = Stockfish(parameters={"Threads": 2})
 
 print('Input moves in the format starting square to ending square.  Do not add symbols for takes/check.  Castle with starting pos of king and ending pos of king')
@@ -22,6 +23,14 @@ while board.outcome() == None: #repeat until the game reaches win/loss/draw
         start = time.perf_counter()
         engine_output = chess_engine.run()
         print('Time Taken: ' + str(time.perf_counter() - start))
+
+        if time.perf_counter() - start < 1:
+            ply += 1
+            chess_engine = ChessEngine(board, ply, 5, False, threads)
+        elif time.perf_counter() - start > 30.5:
+            ply -= 1
+            chess_engine = ChessEngine(board, ply, 5, False, threads)
+
         print('Engine Eval: ' + str(-engine_output[1] / 100))
         #stockfish.set_fen_position(str(board.fen()))
         #board.push(chess.Move.from_uci(stockfish.get_best_move_time(1500)))
@@ -31,6 +40,7 @@ while board.outcome() == None: #repeat until the game reaches win/loss/draw
         print('Invalid Move')
 
 if board.outcome() == chess.Outcome(termination=chess.Termination.CHECKMATE, winner=False):
+    print(board)
     print("I'm sorry, I think you missed it.")
 
 print('Thank you for a very enjoyable game.')
